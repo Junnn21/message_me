e83161184a643b9847ef26df882fd688c339e999
unknown <cjun580@gmail.com>
2022-08-31 13:58:30 +0700

    Complete MessageMe

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,28 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import 'jquery'
 import 'semantic-ui'
+import "channels"
 
-$(document).ready(function(){
+window.scroll_bottom = function() {
+    if ($('#messages').length > 0) {
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    }
+}
+
+window.submit_message = function() {
+    $('#message_body').on('keydown', function(e) {
+        if(e.keyCode == 13) {
+            $('button').click();
+            e.target.value = "";
+        }
+    });
+}
+
+document.addEventListener("turbo:load", () => {
     $('.ui.dropdown') .dropdown();
+    $('.message .close').on('click', function() {
+        $(this).closest('.message').transition('fade');
+    });
+    submit_message();
+    scroll_bottom();
 });
